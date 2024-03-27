f07f14c166949fa83979ebe555448b7732fe9dc0
Efaideleon <109036920+Efai-De-Leon@users.noreply.github.com>
2024-03-27 11:05:35 -0700

    create inventory table header Fixes #8

---
+++ e-commerce-react-vite-app/src/components/data/DataProcessor.tsx
@@ -0,0 +1,17 @@
+import { InventoryData } from "../inventory_table/types"
+
+export function inventoryColumnSums(data: InventoryData[]) {
+	let totalSpent = 0;
+	let totalProfitForSending = 0;
+
+	data.forEach((row) => {
+		totalSpent += row.precio_con_tax; // Sum for 2nd column
+		totalProfitForSending += row.ganancia_para_envio; // Sum for 4th column
+	});
+
+
+	totalSpent = parseFloat(totalSpent.toFixed(2));
+	totalProfitForSending = parseFloat(totalProfitForSending.toFixed(2));
+
+	return { totalSpent, totalProfitForSending };
+}
---
+++ e-commerce-react-vite-app/src/components/inventory_table/InventoryTableTittleBar.tsx
@@ -0,0 +1,17 @@
+import { InventoryTableTitleBarProps } from "./types";
+
+const InventoryTableTitleBar = ({ date, total_spent, total_for_sending }: InventoryTableTitleBarProps) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th> Date: {date}</th>
+                    <th> Total Spent: {total_spent}</th>
+                    <th> Total for Sending: {total_for_sending}</th>
+                </tr>
+            </thead>
+        </table>
+    );
+};
+
+export default InventoryTableTitleBar;
